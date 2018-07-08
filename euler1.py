8453cd902de8e2440d0321bd0a306b2bb8568ebc
Maria <geomar1604@gmail.com>
2018-07-08 13:52:08 +0200

    improved ex1 implementation

--- euler1.py
+++ euler1.py
@@ -1,8 +1,8 @@
 sum = 0
 for x in range(1,1000):
     if x % 3 == 0:
-        sum=sum+x 
+        sum += x 
     elif x % 5 == 0:
-        sum = sum + x 
+        sum += x 
 
-print(sum)
+print('\n\n{}\n\n\n'.format(sum))
