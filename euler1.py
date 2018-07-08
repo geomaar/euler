a35d305e85a6ede27c7d9be92fb9144b45cf8062
Maria <geomar1604@gmail.com>
2018-07-08 14:04:06 +0200

    Added documentation to Euler1

--- euler1.py
+++ euler1.py
@@ -1,8 +1,19 @@
-sum = 0
-for x in range(1,1000):
-    if x % 3 == 0:
-        sum += x 
-    elif x % 5 == 0:
-        sum += x 
+#Find the sum of all the multiples of 3 or 5 below a limit defined by user
+# % is the modulus operator
+
+def sum_3_5(limit):
+    sum = 0
+    for x in range(1, limit):
+        if x % 3 == 0:
+            sum += x 
+        elif x % 5 == 0:
+            sum += x 
+    return sum
+
+limit = input('What is the limit? ')
+limit = int(limit)
+sum = sum_3_5(limit)
 
 print('\n\n{}\n\n\n'.format(sum))
+
+assert sum == 233168
