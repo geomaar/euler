b9fe4b3c17966a4a34fc9a1e783b90031ee200f3
Maria Antonieta Alcantara Rodriguez <geomar1604@gmail.com>
2019-01-11 16:42:09 +0100

    Project Euler 6

--- euler6.py
+++ euler6.py
@@ -9,12 +9,12 @@ numbers and the square of the sum is 3025 − 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred 
 natural numbers and the square of the sum.
 """
-square=0
-sum=0
-for num in range(1,101):
-    square=square+num**2
-    sum=sum+num
-print(square)
-sum_square=(sum**2)
-print(sum_square)
+square = 0
+sum = 0
+for num in range(1, 101):
+    square = square+num**2
+    sum = sum+num
+# print(square)
+sum_square = (sum**2)
+# print(sum_square)
 print(sum_square-square)
